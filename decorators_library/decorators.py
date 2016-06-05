12327abdc2cba219752dd1b1dc4222e9a984f39d
Philipp Horn <philipp.horn4@gmail.com>
2016-06-05 12:58:42 +0000

    Added two decorators with tests

--- decorators_library/decorators.py
+++ decorators_library/decorators.py
@@ -1,4 +1,5 @@
 import time
+import inspect
 from .exceptions import TimeoutError
 from logging import getLogger
 
@@ -21,7 +22,7 @@ def timeout(max_time):
     def decorator(func):
         return Timeout(func, max_time)
     return decorator
-
+    
 def debug(logger=None):
     def other(func):
         def wrapper(*args, **kwargs):
@@ -30,9 +31,10 @@ def debug(logger=None):
                 log = logger
             else:  
                 log = getLogger('tests.test_decorators')
-                log.debug('Executing "{}" with params: {}, {{}}'.format(func.__name__, tuple(args)))
                 
-                log.debug('Finished "{}" execution with result: {}'.format(func.__name__, return_val))
+            log.debug('Executing "{}" with params: {}, {{}}'.format(func.__name__, tuple(args)))
+            log.debug('Finished "{}" execution with result: {}'.format(func.__name__, return_val))
+                
             return return_val
         return wrapper
     return other
@@ -42,6 +44,7 @@ class count_calls(object):
     func_calls = {}
     
     def __init__(self,func):
+        self.func = func
         self.func_name = func.__name__
         self.func_calls.setdefault(self.func_name, 0)
         
@@ -57,9 +60,10 @@ class count_calls(object):
     def reset_counters(self):
         self.func_calls = {}
     
-    def __call__(self):
+    def __call__(self, *args, **kwargs):
         self.func_calls[self.func_name] += 1
-    
+        
+        return self.func(*args, **kwargs)
     
 class memoized:
     def __init__(self, func):
@@ -73,3 +77,53 @@ class memoized:
         return return_val
         
     
+def assert_type(*args, **kwargs):
+    type_list = list(args)
+    type_dict = kwargs
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            
+            if len(type_list) != len(args) or len(type_dict) != len(kwargs):
+                expected = len(inspect.getargspec(func).args)
+                received = len(args) + len(kwargs)
+                raise ValueError("Wrong number arguments. Expected: {}; Received: {}".format(expected, received))
+                
+            type_args = [type(val) for val in args]
+            type_kwargs = [kwargs[key] for key in type_dict] 
+            
+            if type_args != type_list or not all([type_dict[key] == kwargs[key] for key in type_dict]):
+                expected = tuple(list(type_list) + [type_dict[key] for key in type_dict])
+                received = tuple(type_args + type_kwargs)
+                raise TypeError("Wrong type. Expected: {}; Received: {}".format(get_type(expected), get_type(received)))
+                
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+def get_type(type_list):
+    return tuple(type_.__name__ for type_ in type_list) 
+
+class running_time:
+    
+    def __init__(self, func):
+        self.func = func
+        self.running_times = []
+        
+    def __call__(self, *args, **kwargs):
+        start = time.time()
+        return_val = self.func(*args, **kwargs)
+        end = time.time()
+        self.running_times.append(end - start)
+        return return_val
+    
+    def average_time(self):
+        return sum(self.running_times) / len(self.running_times)
+        
+    def counter(self):
+        return len(self.running_times)
+        
+    def min_running_time(self):
+        return min(self.running_times)
+        
+    def max_running_time(self):
+        return max(self.running_times)
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -53,11 +53,11 @@ class DecoratorsTestCase(unittest.TestCase):
     def test_count_calls(self):
         @count_calls
         def my_func():
-           pass
+           return 1
         my_func()
         my_func()
         my_func()
-        my_func()
+        self.assertEqual(my_func(), 1)
         self.assertEqual(my_func.counter(), 4)
         self.assertEqual(count_calls.counters(), {'my_func': 4})
         count_calls.reset_counters()
@@ -101,3 +101,37 @@ class DecoratorsTestCase(unittest.TestCase):
         self.assertEqual(add.cache, {(1, 2): 3, (2, 3): 5})
         self.assertEqual(add(3, 4), 7)
         self.assertEqual(add.cache, {(1, 2): 3, (2, 3): 5, (3, 4): 7})
+
+    def test_assert_type_length(self):
+        @assert_type(int, int)
+        def my_add(a, b):
+            return a + b
+        
+        with self.assertRaisesRegexp(ValueError, 'Wrong number arguments. Expected: 2; Received: 3'):
+            my_add(2, 23, 4)
+        
+    
+    def test_assert_type_different_types(self):
+        @assert_type(int, int)
+        def my_add(a, b):
+            return a + b
+            
+        self.assertEqual(my_add(1,2), 3)
+        with self.assertRaises(TypeError):
+            my_add("asd", 5)
+        # with self.assertRaisesRegexp(TypeError, "Wrong type. Expected: ('int', 'int'); Received: ('str', 'int')"):
+        #     my_add("ads", 2)
+        
+    def test_running_time(self):
+        """tracks the running times for a function"""
+        @running_time
+        def my_add(a, b):
+            return a +b
+            
+        my_add(10,20)
+        my_add(50,20)
+        my_add(1000,4259)
+        
+        self.assertEqual(my_add(1,2), 3)
+        self.assertEqual(my_add.counter(), 4)
+        self.assertTrue(0 < my_add.average_time() <  0.001)
