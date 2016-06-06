1e258412316e3c61725ec07b9d54917c180780b7
Lana <lana519@gmail.com>
2016-06-06 20:18:31 +0000

    all tests passing

--- decorators_library/decorators.py
+++ decorators_library/decorators.py
@@ -24,18 +24,51 @@ from threading import Thread
 #         return wrapper
 #     except TimeoutError:
 #         raise TimeoutError('Function call timed out')
+
+def timeout(seconds):
+    def actual_decorator(function):
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            fast_enough = function(*args, **kwargs)
+            end_time = time.time()
+            print(end_time - start_time) #for debug purposes
+            if end_time - start_time > seconds:
+                raise TimeoutError('Function call timed out')
+            else:
+                return fast_enough
+        return wrapper
+    return actual_decorator
+    
         
-def debug():
+def debug(logger=None):
     def finished_func(func):
+        if not logger:
+            log = logging.getLogger(func.__module__)
+            log.setLevel(logging.DEBUG)
+        else:
+            log = logger
         def _new_func(*args, **kwargs):
-            logging.debug('Executing {} with {}, {}'.format(func, *args, **kwargs))
-            return func(*args, **kwargs)
-        logging.debug('Finished {} executed with result: {}'.format(func, _new_func))
+            arg_list = [x for x in args]
+            log.debug('Executing \"{}\" with params: {}, {}'.format(func.__name__, tuple(arg_list), kwargs or '{}'))
+            result = func(*args, **kwargs)
+            log.debug('Finished \"{}\" execution with result: {}'.format(func.__name__, result))
+            return result
+        result = _new_func
+        return result
     return finished_func
     
-# def memoized():
-#     pass
-
+class memoized(object):
+    def __init__(self, func):
+        self.cache = {}
+        self.func = func
+        
+    def __call__(self, *args):
+        if args in self.cache:
+            return self.cache[args]
+        else:
+            result = self.func(*args)
+            self.cache.setdefault(args, result)
+            return result
 
 def count_calls(wrapped):
     def inner(*args, **kwargs):
@@ -66,6 +99,5 @@ def count_calls(wrapped):
 # @timeout(1)    
 # def func():
 #     time.sleep(2)
-#     print('didnt werk')
+#     print('didnt work :(')
     
-# func()
