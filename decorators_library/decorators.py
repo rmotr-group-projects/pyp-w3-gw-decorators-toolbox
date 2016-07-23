716629724d50a27edd34ed155d3ad95c88f671de
gorrog <spam@gorrog.org>
2016-07-23 15:12:35 +0000

    Implement count_calls class

--- decorators_library/decorators.py
+++ decorators_library/decorators.py
@@ -1,1 +1,28 @@
 # implement your decorators here.
+
+class count_calls(object):
+    
+    counters_dict = {}
+    
+    def __init__(self, function):
+        self.function = function
+        #self.count = 0
+        self.func_name = self.function.__name__
+    
+    def __call__ (self):
+        # if not self.count:
+        if self.func_name not in self.counters_dict.keys():
+            self.__class__.counters_dict[self.func_name] = 0 # instead of cls, use self.__class__
+        self.__class__.counters_dict[self.func_name] += 1
+        return self.function()
+    
+    def counter(self):
+        return  self.__class__.counters_dict[self.func_name]
+        
+    @classmethod
+    def counters(cls):
+        return cls.counters_dict
+    
+    @classmethod
+    def reset_counters(cls):
+        cls.counters_dict = {}
