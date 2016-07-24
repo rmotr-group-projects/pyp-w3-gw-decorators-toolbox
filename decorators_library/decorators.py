ef93ca3b39c335769eb784844ebd435d25137563
gorrog <spam@gorrog.org>
2016-07-24 22:16:38 +0000

    Add completed memoized function

--- decorators_library/decorators.py
+++ decorators_library/decorators.py
@@ -1,3 +1,4 @@
+import logging
 # implement your decorators here.
 
 class count_calls(object):
@@ -6,18 +7,16 @@ class count_calls(object):
     
     def __init__(self, function):
         self.function = function
-        #self.count = 0
         self.func_name = self.function.__name__
-    
-    def __call__ (self):
-        # if not self.count:
         if self.func_name not in self.counters_dict.keys():
             self.__class__.counters_dict[self.func_name] = 0 # instead of cls, use self.__class__
+    
+    def __call__ (self):
         self.__class__.counters_dict[self.func_name] += 1
         return self.function()
     
     def counter(self):
-        return  self.__class__.counters_dict[self.func_name]
+        return  self.__class__.counters_dict.get(self.func_name, 0)
         
     @classmethod
     def counters(cls):
@@ -26,3 +25,54 @@ class count_calls(object):
     @classmethod
     def reset_counters(cls):
         cls.counters_dict = {}
+        
+class memoized(object):
+    cache = {}
+    # {     "add":  {
+    #                   (1, 2, 3): 1,
+    #                   (4, 5, 6): 1
+    #               }
+    # }
+    
+    def __init__(self, function):
+        self.function = function
+        #self.count = 0
+        self.func_name = self.function.__name__
+        self.cache = {}
+                        #structure for self.cache should be:
+                            # {
+                    #                   (1, 2, 3): 1,
+                    #                   (4, 5, 6): 1
+                    #               }
+    
+    def __call__ (self, *args):
+        args_tuple = tuple(args)
+        if self.cache and self.cache.get(args_tuple, False): # if both function and parameters are stored in cache
+            return self.cache[args_tuple] # return cached result
+            
+        result = self.function(*args)
+        
+        if self.cache and (args_tuple not in self.cache): # if function is cached but NOT parameters
+            self.cache[args_tuple] = result
+            return result
+        else: # if function has not been stored in cache previously
+            self.cache = {args_tuple: result}
+            return result
+    
+   
+class debug(object):
+    pass
+    # def init(self, function):
+    #     self.logger = logging.getLogger(function.__name__)
+    #     self.logger.setLevel(logging.DEBUG)
+    
+    # def __call__(self, *args, **kwargs):
+    #     self.logger.debug('Executing {1} with params: {2}, {3}')
+    #     pass
+    #
+    #
+    #
+    #
+    #
+    #
+   
