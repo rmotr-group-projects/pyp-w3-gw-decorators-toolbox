a93749698c336c942344eccf3f94ea7a61e38594
Justin Wade <jivewade@gmail.com>
2016-09-10 18:06:24 +0000

    All tests pass!

--- decorators_library/decorators.py
+++ decorators_library/decorators.py
@@ -1,3 +1,8 @@
+import signal
+from .exceptions import TimeoutError
+from functools import wraps
+import logging
+
 # implement your decorators here.
 class count_calls(object):
     cls_counter = {}
@@ -36,3 +41,40 @@ class memoized(object):
             value = self.func(*args)
             self.cache[args] = value
             return value
+            
+    
+def timeout(seconds = 10):
+    "Simpel decorator to stop after certain time"
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            signal.signal(signal.SIGALRM, signal_handler)
+            signal.alarm(seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+            return result
+        return wrapper 
+    return decorator
+
+
+def signal_handler(signum, frame):
+    raise TimeoutError("Function call timed out.")
+    
+def debug(logger=None):
+    log = logger
+    def real_dec(function):
+        if not log:
+            logger = logging.getLogger(function.__module__)
+        else:
+            logger=log
+        def wrapper(*args, **kwargs):
+            return_val = function(*args, **kwargs)
+            logger.debug('Executing "{}" with params: {}, {{}}'.format(function.__name__,\
+            tuple(args)))
+            logger.debug('Finished "{}" execution with result: {}'.format(function.__name__,\
+            return_val))
+            return return_val
+        return wrapper
+    return real_dec
