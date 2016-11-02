19a66d6ee3a7d95e0cb13e3fb3f231dd6ca25d41
Alex Gable <alex.gable@icloud.com>
2016-11-02 00:54:17 +0000

    fully functional logger

--- decorators_library/decorators.py
+++ decorators_library/decorators.py
@@ -5,15 +5,13 @@ import sys
 def debug(logger=None):
     
     def log_function(original_function):
-        # 
-        logger = logging.getLogger(original_function.__module__)
-        logger.setLevel(logging.DEBUG)
-        
+
         def wrapper(*args, **kwargs):
-            if logger != 'error_logger':
-                
-                logger.debug('Executing \"%s\" with params: %s, %s', original_function.__name__, args, kwargs)
-                logger.debug('Finished \"%s\" execution with result: %s', original_function.__name__, original_function(*args))
+            if logger is None:
+                debug_logger = logging.getLogger(original_function.__module__)
+                debug_logger.setLevel(logging.DEBUG)
+                debug_logger.debug('Executing \"%s\" with params: %s, %s', original_function.__name__, args, kwargs)
+                debug_logger.debug('Finished \"%s\" execution with result: %s', original_function.__name__, original_function(*args))
             
             return original_function(*args, **kwargs)
         
@@ -21,12 +19,6 @@ def debug(logger=None):
         
     return log_function
 
-#@debug(logger='error_logger')
-@debug()
-def my_add(a, b):
-    return a + b
-
-my_add(1,2)
 
 #timeout
 
