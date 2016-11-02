f2dcbebbad10fa139ad91985fdd36ff4d66b8a46
Alex Gable <alex.gable@icloud.com>
2016-11-02 01:50:17 +0000

    timeout decorator is timing out like a champ

--- decorators_library/decorators.py
+++ decorators_library/decorators.py
@@ -1,5 +1,8 @@
 import logging 
-import sys
+import time
+import signal
+from .exceptions import *
+
 
 #debug
 def debug(logger=None):
@@ -21,6 +24,50 @@ def debug(logger=None):
 
 
 #timeout
+def timeout(sec=None):
+    if sec is None:
+        raise AttributeError('You need to provide a number of seconds.')
+    else:
+        
+        def decorator(original_function):
+            
+            def raise_error(signum, frame):
+                raise TimeoutError('Function call timed out')
+                #print("ERROR")
+            
+            signal.signal(signal.SIGALRM, raise_error)
+            signal.alarm(sec)
+            
+            def wrapper(*args, **kwargs):
+                
+                return original_function(*args, **kwargs)
+            
+            return wrapper
+            
+        signal.alarm(0)
+    
+        return decorator
+        
+        """
+        record start time
+        run function
+        record stop time
+        
+        time_to_run = stop_time - start_time
+        
+
+       if time_to_run > sec:
+            raise TimeoutError('Function call timed out')
+        """
+"""
+@timeout(1)
+def my_add(a,b):
+    time.sleep(2)
+    return a + b
+    
+print(my_add(1,2))
+"""
+
 
 #count_calls as a class
 
