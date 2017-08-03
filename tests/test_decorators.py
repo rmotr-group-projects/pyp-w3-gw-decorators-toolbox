6931639bea9939870590349e8cd05b86b37c02cc
Santiago Basulto <santiago.basulto@gmail.com>
2017-08-02 20:50:57 -0400

    Inspect tests + debug is optional

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,14 +1,31 @@
 # -*- coding: utf-8 -*-
+import sys
 import time
 import unittest
 import logging
-from mock import patch
 
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+from mock import patch
 from testfixtures import LogCapture
 
 from decorators_library.decorators import (
-    timeout, memoized, debug, count_calls)
+    timeout, memoized, count_calls, inspect)
 from decorators_library.exceptions import FunctionTimeoutException
+
+
+class CaptureOutput(list):
+    def __enter__(self):
+        self._stdout = sys.stdout
+        sys.stdout = self._stringio = StringIO()
+        return self
+    def __exit__(self, *args):
+        self.extend(self._stringio.getvalue().splitlines())
+        del self._stringio
+        sys.stdout = self._stdout
 
 
 class TimeoutDecoratorTestCase(unittest.TestCase):
@@ -44,33 +61,48 @@ class TimeoutDecoratorTestCase(unittest.TestCase):
             very_slow_function()
 
 
-class DebugDecoratorTestCase(unittest.TestCase):
-    def test_debug_default_logger(self):
-        @debug()
+class InspectDecoratorTestCase(unittest.TestCase):
+    def test_inspect_with_args(self):
+
+        @inspect
         def my_add(a, b):
             return a + b
 
-        with LogCapture() as capture:
-            res = my_add(1, 2)
-            capture.check(
-                ('tests.test_decorators', 'DEBUG', 'Executing "my_add" with params: (1, 2), {}'),
-                ('tests.test_decorators', 'DEBUG', 'Finished "my_add" execution with result: 3')
-            )
-        self.assertEqual(res, 3)
+        with CaptureOutput() as output:
+            res1 = my_add(1, 2)
+            res2 = my_add(5, 3)
 
-    def test_debug_custom_logger(self):
-        logging.basicConfig()
-        error_logger = logging.getLogger('test_decorators.error_logger')
-        error_logger.setLevel(logging.ERROR)
+        self.assertEqual(res1, 3)
+        self.assertEqual(res2, 8)
 
-        @debug(logger=error_logger)
-        def my_add(a, b):
-            return a + b
+        self.assertEqual(len(output), 2)
+        self.assertEqual(output, [
+            "my_add invoked with 1, 2. Result: 3",
+            "my_add invoked with 5, 3. Result: 8",
+        ])
 
-        with LogCapture() as capture:
-            res = my_add(1, 2)
-            capture.check()  # nothing was logged
-        self.assertEqual(res, 3)
+    def test_inspect_with_args_and_kwargs(self):
+
+        @inspect
+        def calculate(a, b, operation='add'):
+            if operation == 'add':
+                return a + b
+            if operation == 'subtract':
+                return a - b
+
+        with CaptureOutput() as output:
+            res1 = calculate(7, 2)
+            res2 = calculate(9, 6, operation='subtract')
+
+        self.assertEqual(res1, 9)
+        self.assertEqual(res2, 3)
+
+        self.assertEqual(len(output), 2)
+
+        self.assertEqual(output, [
+            "calculate invoked with 7, 2. Result: 9",
+            "calculate invoked with 9, 6, operation=subtract. Result: 3",
+        ])
 
 
 class CountCallsDecoratorTestCase(unittest.TestCase):
@@ -130,3 +162,35 @@ class MemoizedDecoratorTestCase(unittest.TestCase):
 
         with patch.dict(add.cache, {(1, 2): 6}):
             self.assertEqual(add(1, 2), 6, "Not using cached value")
+
+
+# from decorators_library.decorators import debug
+#
+#
+# class DebugDecoratorTestCase(unittest.TestCase):
+#     def test_debug_default_logger(self):
+#         @debug()
+#         def my_add(a, b):
+#             return a + b
+#
+#         with LogCapture() as capture:
+#             res = my_add(1, 2)
+#             capture.check(
+#                 ('tests.test_decorators', 'DEBUG', 'Executing "my_add" with params: (1, 2), {}'),
+#                 ('tests.test_decorators', 'DEBUG', 'Finished "my_add" execution with result: 3')
+#             )
+#         self.assertEqual(res, 3)
+#
+#     def test_debug_custom_logger(self):
+#         logging.basicConfig()
+#         error_logger = logging.getLogger('test_decorators.error_logger')
+#         error_logger.setLevel(logging.ERROR)
+#
+#         @debug(logger=error_logger)
+#         def my_add(a, b):
+#             return a + b
+#
+#         with LogCapture() as capture:
+#             res = my_add(1, 2)
+#             capture.check()  # nothing was logged
+#         self.assertEqual(res, 3)
