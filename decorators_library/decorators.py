8f369d67424b62762d5c8f12f8a75cccee5e297a
Khristopher Chireno <kcmastermind2005@gmail.com>
2017-10-28 18:46:25 +0000

    inspect and count_calls completed

--- decorators_library/decorators.py
+++ decorators_library/decorators.py
@@ -1,1 +1,50 @@
 # implement your decorators here.
+def inspect(fn):
+    def new_fn(*args, **kwargs):
+        arg_list = []
+        result = fn(*args, **kwargs)
+        for arg in args:
+            arg_list.append(str(arg))
+        for kwarg in kwargs:
+            arg_list.append(str(kwarg)+'='+kwargs[kwarg])
+        print ('{} invoked with {}. Result: {}'.
+               format(fn.__name__, ', '.join(arg_list), result))
+        return result
+    return new_fn
+
+
+class count_calls(object):
+    counter_cache = {}
+
+    def __init__(self, fn):
+        self.fn = fn
+        count_calls.counter_cache[self.fn.__name__] = 0
+        self.count = count_calls.counter_cache[self.fn.__name__]
+
+    def __call__(self):
+        count_calls.counter_cache[self.fn.__name__] += 1
+
+    def counter(self):
+        return count_calls.counter_cache[self.fn.__name__]
+
+    @classmethod
+    def reset_counters(cls):
+        cls.counter_cache.clear()
+
+    @classmethod
+    def counters(cls):
+        return cls.counter_cache
+
+
+def timeout(fn):
+    pass
+
+
+class memoized(object):
+    def __init__(self, fn):
+        self.fn = fn
+        self.cache = {}
+    
+    def __call__(self):
+        
+    
