ffb2265ae89cce894413bc33ed91ec0993ae501a
Erika Boren <eboren@gmail.com>
2017-10-28 01:51:12 +0000

    add inspect decorator

--- decorators_library/decorators.py
+++ decorators_library/decorators.py
@@ -1,1 +1,10 @@
 # implement your decorators here.
+def inspect(func):
+   
+  def func_wrapper(*args):
+    print ("my_add invoked with {}. Result: {}".format(str(args)[1:-1],func(*args)))
+  return func_wrapper  
+  
+@inspect
+def my_add(a, b):
+    return a + b
