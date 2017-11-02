d86d9889c8f9ddf400a94598517b645764d13c93
Kelli Blalock <kelli.blalock2@gmail.com>
2017-11-02 23:52:47 +0000

    Pass InspectDecoratorTestCase

--- decorators_library/decorators.py
+++ decorators_library/decorators.py
@@ -2,12 +2,24 @@
 def timeout():
     pass
 
+
 def memoized():
     pass
+
 
 def count_calls():
     pass
 
-def inspect():
-    pass
-    
+
+def inspect(fn):
+    def wrapper(*args, **kwargs):
+        formatted_kwargs = []
+        for key, value in kwargs.items():
+            formatted_kwargs.append('{}={}'.format(key, value))
+        all_args = list(args) + formatted_kwargs
+        all_args = ', '.join(str(arg) for arg in all_args)
+        result = fn(*args, **kwargs)
+        print('{} invoked with {}. Result: {}'.format(fn.__name__, all_args,
+                                                      result))
+        return fn(*args, **kwargs)
+    return wrapper
