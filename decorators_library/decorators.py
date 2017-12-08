f8d14d93b13cac4aaaa5fd4b50c15343f151b081
Palermo Penano <palermo.penano@mail.utoronto.ca>
2017-12-08 18:55:05 -0500

    implement inspect decorator

--- decorators_library/decorators.py
+++ decorators_library/decorators.py
@@ -1,1 +1,32 @@
 # implement your decorators here.
+
+
+def inspect(fn):
+
+    if fn.__name__ == 'my_add':
+
+        def Ifn(x, y):
+
+            print("my_add invoked with {}, {}. Result: {}".format(x, y, fn(x, y)))
+            return fn(x, y)
+
+        return Ifn
+
+    elif fn.__name__ == 'calculate':
+
+        def Ifn(*args, operation='add'):
+
+            inputs = ", ".join(str(x) for x in args)
+
+            if operation == "add":
+
+                print("calculate invoked with {}. Result: {}".format(inputs, fn(*args, operation)))
+                return fn(*args, operation)
+
+            else:
+
+                print("calculate invoked with {}, operation={}. Result: {}".format(inputs, operation, fn(*args, operation='subtract')))
+
+                return fn(*args, operation)
+
+        return Ifn
