7f4b4cd94f3bffb2b93086fcd4490919d274c0d0
Ilaria Varriale <ilariasvarriale@gmail.com>
2017-12-24 21:16:30 +0000

    Add timeout and debug decorators

--- decorators_library/decorators.py
+++ decorators_library/decorators.py
@@ -1,5 +1,7 @@
 import functools
 import collections
+import signal
+from decorators_library.exceptions import FunctionTimeoutException
 
 def inspect(function):
     @functools.wraps(function)
@@ -12,6 +14,23 @@ def inspect(function):
             print('{} invoked with {}, {}, operation={}. Result: {}'.format(function.__name__, x, y, operation, result))
         return result
     return wrapper
+
+class timeout(object):
+    def __init__(self, duration, exception=FunctionTimeoutException):
+        self.duration = duration
+        self.exception = exception
+    
+    def raise_alarm(self, signum, stack):
+        raise self.exception("Function call timed out")
+
+    def __call__(self, function):
+        @functools.wraps(function)
+        def wrapped(*args, **kwargs):
+            signal.signal(signal.SIGALRM, self.raise_alarm)
+            signal.alarm(self.duration)
+            return function(*args, **kwargs)
+        signal.alarm(0)
+        return wrapped
 
 
 class memoized(object):
@@ -29,6 +48,7 @@ class memoized(object):
             self.cache[args] = value
             return value
 
+
 class count_calls(object):
     FUNCTION_COUNTERS = {}
     
@@ -36,7 +56,7 @@ class count_calls(object):
         self.function = function
         self.calls = 0
         count_calls.FUNCTION_COUNTERS[function] = self
-        
+
     
     def __call__(self, *args, **kwargs):
         self.calls += 1
@@ -49,10 +69,27 @@ class count_calls(object):
     def counters(cls):
         return dict([(f.__name__, cls.FUNCTION_COUNTERS[f].calls)
                      for f in cls.FUNCTION_COUNTERS])
-    
+
     @classmethod
     def reset_counters(cls):
         cls.FUNCTION_COUNTERS = {}
 
-def timeout():
-    pass
+
+class debug(object):
+    def __init__(self, logger=None):
+        self.logger = logger
+  
+    def __call__(self, fn):
+        @wraps(fn)
+        def new_fn(*args, **kwargs):
+            kwarg_lst = ', '.join(['{}={}'.format(key, val) for key, val in kwargs.items()])
+            arg_str = ', '.join(map(str,args))
+            
+            if not self.logger:
+                self.logger = logging.getLogger('tests.test_decorators')
+            
+            self.logger.debug('Executing "{}" with params: ({}), {{{}}}'.format(fn.__name__, arg_str, kwarg_lst))
+            fn_result = fn(*args, **kwargs)
+            self.logger.debug('Finished "{}" execution with result: {}'.format(fn.__name__, str(fn_result)))
+            return fn_result
+        return new_fn
