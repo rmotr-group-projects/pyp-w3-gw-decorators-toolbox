7eb0389ebda666ef49b358088bba14546e64155b
William <luwp@hotmail.com>
2018-01-02 17:57:30 +0000

    second commit

--- decorators_library/decorators.py
+++ decorators_library/decorators.py
@@ -1,29 +1,32 @@
 import time
 from functools import wraps
+import signal
+import logging 
+
 from decorators_library.exceptions import FunctionTimeoutException
 
-# class FunctionTimeoutException(Exception):
-#     pass
+def inspect(org_func):
+    def wrapped(*args, **kwargs):
+        org_r = org_func(*args, **kwargs)
+        org_func_name = org_func.__name__
+        if len(kwargs) > 0: 
+            print("{} invoked with {}, {}, operation={}. Result: {}".format(org_func_name, args[0], args[1], kwargs['operation'], org_r))
+        else: 
+            print("{} invoked with {}, {}. Result: {}".format(org_func_name, args[0], args[1], org_r))
+        return org_r
+    return wrapped
 
-
-def inspect(org_fun):
-    def wrapper(*args):
-        s = ','.join([str(a) for a in args])
-        return "{} invoked with {} Result: {}".format(org_fun.__name__, s, org_fun(*args))
-    return wrapper
-    
 # @inspect
 # def my_add(a, b):
 #     return a + b
 
-# print(my_add(3, 5))  # 8
+# print(my_add(a=3, b=5))  # 8
 # Printed: "my_add invoked with 3, 5. Result: 8"
 
 
 
 
-
-def timeout(time_limit, exception = FunctionTimeoutException, error_message='Function call timed out'):
+def timeout_old(time_limit, exception = FunctionTimeoutException, error_message='Function call timed out'):
     def real_decorator(org_fun):
         @wraps(org_fun)
         def wrapper(*args, **kwargs):
@@ -35,6 +38,24 @@ def timeout(time_limit, exception = FunctionTimeoutException, error_message='Fun
             return 
         return wrapper
     return real_decorator
+
+# reference: https://pymotw.com/2/signal/
+class timeout(object):
+    def __init__(self, time_limit, exception=FunctionTimeoutException):
+        self.time_limit = time_limit 
+        self.exception = exception 
+        
+    def receive_alarm(self, signum, stack):
+        raise self.exception("Function call timed out")
+    
+    def __call__(self, org_func):
+        def wrapper(*args, **kwargs):
+            signal.signal(signal.SIGALRM, self.receive_alarm)
+            signal.alarm(self.time_limit)
+            org_r = org_func(*args, **kwargs)
+            signal.alarm(0)
+            return org_r 
+        return wrapper 
 
 # @timeout(1)
 # def very_slow_function():
@@ -54,19 +75,22 @@ def timeout(time_limit, exception = FunctionTimeoutException, error_message='Fun
 
 
 
+
+# reference: https://pymotw.com/3/logging/
 class debug(object):
-    def __init__(self):
-        pass
-
-    def __call__(self, org_fun):
-        @wraps(org_fun)
-        def wrapper(*args, **kwargs):
-            print("Executing '{}' with params: {} {}".format(org_fun.__name__, args, kwargs))
-            result = org_fun(*args, **kwargs)
-            print("Finished '{}' execution with result: {}".format(org_fun.__name__, result))
-            return
-        return wrapper
-
+    def __init__(self, logger=None):
+        self.logger = logger 
+    
+    def __call__(self, org_func):
+        def wrapper(*args, **kwargs): 
+            org_func_name = org_func.__name__
+            org_r = org_func(*args, **kwargs)
+            if self.logger is None: 
+                self.logger = logging.getLogger(org_func.__module__)
+            self.logger.debug('Executing "{}" with params: {}, {}'.format(org_func_name, args, kwargs))
+            self.logger.debug('Finished "{}" execution with result: {}'.format(org_func_name, org_r))
+            return org_r
+        return wrapper 
 
 # @debug()
 # def my_add(a, b):
@@ -78,29 +102,27 @@ class debug(object):
 
 
 
-class count_calls(object):
-    FUNCTION_COUNTERS = {}
-
+class count_calls(object): 
+    function_count = {}
+    
     def __init__(self, org_fun):
-        self.org_fun = org_fun
-        self.num_of_calls = 0
-        count_calls.FUNCTION_COUNTERS[org_fun] = self
-
-    def __call__(self):
-        self.num_of_calls += 1        
-        @wraps
-        def wrapper(*args, **kwargs):
-            result = self.org_fun(*args, **kwargs)
-            return result
-        return wrapper        
-
-    def counter(self):
-        return self.num_of_calls
-
+        self.org_fun = org_fun 
+        self.function_count[self.org_fun.__name__] = 0
+        
+    def __call__(self, *args, **kwargs):
+        self.function_count[self.org_fun.__name__] += 1 
+        return self.org_fun(*args, **kwargs) 
+    
+    def counter(self): 
+        return self.function_count[self.org_fun.__name__]
+        
     @classmethod
     def counters(cls):
-        return dict([(f.__name__, cls.FUNCTION_COUNTERS[f].num_of_calls) for f in cls.FUNCTION_COUNTERS])
-
+        return cls.function_count 
+    
+    @classmethod 
+    def reset_counters(cls):
+        cls.function_count = {} 
 
 # @count_calls
 # def my_func():
@@ -128,7 +150,6 @@ class memoized(object):
             value = self.org_fun(*args)
             self.cache[args] = value
             return value
-
 
 # @memoized
 # def add(a, b):
